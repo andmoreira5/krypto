f0a5c2230622f60b4f256ae71bcf100ae9bd9233
Anderson Moreira <and.moreira5@gmail.com>
2026-04-14 16:44:12 -0300

    chore: initial setup with vite, typescript, tailwind and pnpm

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black flex h-screen w-screen justify-center items-center">
+      <h1 className=" text-white font-bold text-2xl">
+        STARTING THE KRYPTO PROJECT!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
